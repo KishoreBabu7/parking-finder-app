122fe5c5b5bec455f6335d1e12096f74ecf38f5c
Tulugu Kishore Babu <tulugukishorebabu@gmail.com>
2024-09-30 13:25:56 +0530

    changing names of places

--- src/app/parking/parking.component.ts
+++ src/app/parking/parking.component.ts
@@ -18,14 +18,14 @@ export class ParkingComponent {
 
   parkingSpots: ParkingSpot[] = [
     {
-      name: 'Spot 1',
-      location: 'Location 1',
+      name: 'Paralakhemundi',
+      location: 'Centurion University Of Technology and management, Alluri Nagar, Odisha',
       availability: 10,
       mapUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4809.668770448797!2d84.12824941211053!3d18.798192684282068!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a3cf98111e52f3b%3A0x316b288d3abda74e!2sCenturion%20University%20of%20Technology%20and%20Management%20(CUTM)!5e0!3m2!1sen!2sin!4v1727674534052!5m2!1sen!2sin',
     },
     {
-      name: 'Spot 2',
-      location: 'Location 2',
+      name: 'Paralakhemundi',
+      location: 'R Sitapur Post Office, Kshatriya St, R.Sitapur, Tulasipadu, Odisha 761211',
       availability: 5,
       mapUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4809.681704984787!2d84.12622740531938!3d18.797740012642826!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a3cf9902341708b%3A0xf2b1b0f0980233a0!2sR%20Sitapur%20Post%20Office!5e0!3m2!1sen!2sin!4v1727680689072!5m2!1sen!2sin',
     }
