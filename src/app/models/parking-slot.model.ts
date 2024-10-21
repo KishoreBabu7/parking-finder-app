6d8b0b9f1e726f6aae7cbb6a03133f19e386c480
ModiSireesha <modisireesha09@gmail.com>
2024-10-21 23:19:35 +0530

    updated

--- src/app/models/parking-slot.model.ts
+++ src/app/models/parking-slot.model.ts
@@ -1,6 +1,6 @@
 export interface ParkingSlot {
-  id: number; // Unique identifier for the slot
-  name: string; // Name of the slot
-  booked: boolean; // Indicates if the slot is booked
-  parkingSpotId: number; // Foreign key to the associated parking spot
+  id: number;
+  name: string;
+  booked: boolean;
+  parkingSpotId: number;
 }
--- src/app/models/parking-spot.model.ts
+++ src/app/models/parking-spot.model.ts
@@ -1,10 +1,10 @@
 import { ParkingSlot } from './parking-slot.model'; // Adjust the path as needed
 
 export interface ParkingSpot {
-  id: number; // Unique identifier for the parking spot
-  name: string; // Name of the parking spot
-  location: string; // Location of the parking spot
-  availability: number; // Number of available slots
-  mapUrl: string; // URL for the map location
-  parkingSlots: ParkingSlot[]; // Array of associated parking slots
+  id: number; 
+  name: string;
+  location: string;
+  availability: number;
+  mapUrl: string;
+  parkingSlots: ParkingSlot[];
 }
