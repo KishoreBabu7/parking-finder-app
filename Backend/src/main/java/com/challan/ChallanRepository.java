c23589678897800f1347c95a167e1f842a9a6a5b
Rokkam Divya <rokkamdivya7@gmail.com>
2024-11-08 10:45:14 +0530

    updated

---
+++ Backend/src/main/java/com/challan/ChallanRepository.java
@@ -0,0 +1,8 @@
+package com.challan;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ChallanRepository extends JpaRepository<Challan, Long> {
+}
