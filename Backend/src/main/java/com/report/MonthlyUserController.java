ebb0347af73cc9d01812b34e794d8a6fbd12a37f
Gayatri Pisya <gayatripisya@gmail.com>
2024-11-12 21:28:24 +0530

    updated

--- Backend/src/main/java/com/report/MonthlyUserController.java
+++ Backend/src/main/java/com/report/MonthlyUserController.java
@@ -1,5 +1,4 @@
 package com.report;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:4200") // Allow CORS for Frontend
+@CrossOrigin(origins = "http://localhost:4200") // Allow CORS for your Angular app
 public class MonthlyUserController {
 
     @Autowired
