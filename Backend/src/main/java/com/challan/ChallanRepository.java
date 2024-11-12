187d6cfff7b57da9203ae10aa127f1937c732811
sireesha0904 <modisireesha09@gmail.com>
2024-11-12 18:14:46 +0530

    updated

--- Backend/src/main/java/com/challan/ChallanRepository.java
+++ Backend/src/main/java/com/challan/ChallanRepository.java
@@ -1,6 +1,7 @@
 package com.challan;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
+@Repository
 public interface ChallanRepository extends JpaRepository<Challan, String> {  // String as ID type
 }
