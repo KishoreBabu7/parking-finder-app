b35e0cc5fcda1dfe49be21f07addb1f358eb5a75
sireesha0904 <modisireesha09@gmail.com>
2024-11-12 18:35:41 +0530

    updated

--- Backend/src/main/java/com/main/ParkingSpaceProjectApplication.java
+++ Backend/src/main/java/com/main/ParkingSpaceProjectApplication.java
@@ -1,13 +1,16 @@
-	package com.main;
-	
-	import org.springframework.boot.SpringApplication;
-	import org.springframework.boot.autoconfigure.SpringBootApplication;
-	
-	@SpringBootApplication
-	public class ParkingSpaceProjectApplication {
-	
-		public static void main(String[] args) {
-			SpringApplication.run(ParkingSpaceProjectApplication.class, args);
-		}
-	
-	}
+package com.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.user", "com.challan", "com.parkingspace", "com.payment"})
+@EntityScan(basePackages = {"com.user", "com.challan", "com.parkingspace", "com.payment"})
+public class ParkingSpaceProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingSpaceProjectApplication.class, args);
+    }
+}
